f04dc1c1bdfb01b2979cbfc07d36a02743463215
Profesor Leonardo <leon.romeroaguilera@gmail.com>
2022-07-12 15:29:33 -0400

    fix table

--- database/migrations/2022_07_12_192819_create_table_users.php
+++ database/migrations/2022_07_12_192819_create_table_users.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableUser extends Migration
+class CreateTableUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -21,10 +20,6 @@ class CreateTableUser extends Migration
             $table->string('password');
             $table->timestamps();
         });
-
-        $user = new User();
-        $user->name = "Persona";
-        $user->save();
     }
 
     /**
@@ -34,6 +29,6 @@ class CreateTableUser extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('User');
+        Schema::dropIfExists('users');
     }
 }
